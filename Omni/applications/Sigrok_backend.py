db524edccb002efc61969f8ad7cb7b2241ea75a3
Erick Bacurau <erickbacurau1989@gmail.com>
2024-02-11 20:37:10 +0100

    added error handling

--- Omni/applications/Sigrok_backend.py
+++ Omni/applications/Sigrok_backend.py
@@ -1,22 +1,45 @@
 import socket
 import logging
 import Omni.applications.Sigrok_cli as Sigrok_cli
+import json
 
 _bufsize = 4096
 
+_error_msg_not_json = "Invalid test command. Received test command is not valid JSON."
+_error_msg_bad_dict = "Invalid test command. Received test dictionary command does not contain command key."
 
-def handle_omni_connection(client_socket, client_address):
+
+def handle_omni_connection(client_connection):
+    client_socket, client_address = client_connection
     logging.info(f"Connection established with {client_address}")
     data = client_socket.recv(_bufsize)
-    data_string = data.decode('utf-8')
-    logging.debug(f"Received command: {data_string}")
-    cmd_instance = Sigrok_cli.SigrokCli()
-    ret = cmd_instance.send_cmd(data_string)
-    if (ret["status"] == "done"):
-        client_socket.sendall("done".encode())
+    command_str = data.decode('utf-8')
+    logging.debug(f"Received test command: {command_str}")
+    valid_cmd = __validade_cmd(command_str, client_socket)
+    if (valid_cmd == True):
+        cmd_instance = Sigrok_cli.SigrokCli()
+        ret = cmd_instance.send_cmd(command_str)
+        if (ret["status"] == "done"):
+            client_socket.sendall("done".encode())
     # Hanlde command
     # Send command to sigrok-cli
     client_socket.close()
+
+
+def __validade_cmd(command_str, client_socket):
+    try:
+        command_dict = json.loads(command_str)
+    except json.JSONDecodeError:
+        error_msg = f"{_error_msg_not_json} Received: {command_str}"
+        logging.error(error_msg)
+        client_socket.sendall(error_msg.encode())
+        return False
+    if not 'command' in command_dict:
+        error_msg = f"{_error_msg_bad_dict} Received: {command_str}"
+        logging.error(error_msg)
+        client_socket.sendall(error_msg.encode())
+        return False
+    return True
 
 
 def sigrok_process(port, log_file_path):
@@ -29,6 +52,6 @@ def sigrok_process(port, log_file_path):
     server_socket.listen(1)
     logging.info(f"Sigrok host application listening at {'localhost'}:{port}")
     logging.info("Waiting for connection...")
-    client_socket, address = server_socket.accept()
-    handle_omni_connection(client_socket, address)
+    client_connection = server_socket.accept()
+    handle_omni_connection(client_connection)
     return
--- Omni/tests/unit_tests/test_sigrok_backend.py
+++ Omni/tests/unit_tests/test_sigrok_backend.py
@@ -5,6 +5,7 @@ import shutil
 import os
 import pytest
 from unittest.mock import MagicMock
+import json
 
 
 @pytest.fixture
@@ -48,6 +49,8 @@ def create_temp_folder():
 
 def test_sigrok_backend_binds_socket(socket_mock):
     server_socket, client_socket = socket_mock
+    dummy_cmd__str = json.dumps(command_dict)
+    client_socket.recv.return_value = dummy_cmd__str.encode()
     sigrok_process(1234, log_file_path)
     server_socket.return_value.bind.assert_called_once_with(
         ('localhost', 1234))
@@ -57,6 +60,9 @@ def test_sigrok_backend_binds_socket(socket_mock):
 
 
 def test_sigrok_backend_logs_bind_msgs(socket_mock, logging_mock):
+    server_socket, client_socket = socket_mock
+    dummy_cmd__str = json.dumps(command_dict)
+    client_socket.recv.return_value = dummy_cmd__str.encode()
     sigrok_process(1234, log_file_path)
     logging_mock.info.assert_any_call(
         'Sigrok host application listening at localhost:1234')
@@ -66,34 +72,72 @@ def test_sigrok_backend_logs_bind_msgs(socket_mock, logging_mock):
 
 def test_sigrok_backend_waits_for_data(socket_mock):
     server_socket, client_socket = socket_mock
+    dummy_cmd__str = json.dumps(command_dict)
+    client_socket.recv.return_value = dummy_cmd__str.encode()
     sigrok_process(1234, log_file_path)
     client_socket.recv.assert_called_once_with(4096)
 
 
-@pytest.mark.parametrize("dummy_cmd", ["Sigrok dummy cmd", "Another dummy cmd"])
-def test_sigrok_backend_recv_data(socket_mock, logging_mock, dummy_cmd):
+command_dict = {
+    "command": "Sigrok dummy cmd",
+}
+
+command_dict2 = {
+    "command": "--version",
+}
+
+bad_dict_cmd = {
+    "bad_dict": "a bad string",
+}
+
+
+@pytest.mark.parametrize("dummy_cmd_dict", [command_dict, command_dict2])
+def test_sigrok_backend_recv_data(socket_mock, logging_mock, dummy_cmd_dict):
     server_socket, client_socket = socket_mock
-    client_socket.recv.return_value = dummy_cmd.encode()
+    dummy_cmd__str = json.dumps(dummy_cmd_dict)
+    client_socket.recv.return_value = dummy_cmd__str.encode()
     sigrok_process(1234, log_file_path)
     logging_mock.debug.assert_called_with(
-        f'Received command: {dummy_cmd}')
+        f'Received test command: {dummy_cmd__str}')
+
+
+def test_sigrok_backend_recv_data_logs_error_on_non_json_cmd(socket_mock, logging_mock):
+    server_socket, client_socket = socket_mock
+    dummy_cmd__str = "This is not a JSON string."
+    client_socket.recv.return_value = dummy_cmd__str.encode()
+    sigrok_process(1234, log_file_path)
+    logging_mock.error.assert_called_with(
+        f"Invalid test command. Received test command is not valid JSON. Received: {dummy_cmd__str}")
+    client_socket.sendall.assert_called_with(
+        f"Invalid test command. Received test command is not valid JSON. Received: {dummy_cmd__str}".encode())
+
+
+def test_sigrok_backend_recv_data_logs_error_on_malformed_cmd_dict(socket_mock, logging_mock):
+    server_socket, client_socket = socket_mock
+    dummy_cmd__str = json.dumps(bad_dict_cmd)
+    client_socket.recv.return_value = dummy_cmd__str.encode()
+    sigrok_process(1234, log_file_path)
+    logging_mock.error.assert_called_with(
+        f"Invalid test command. Received test dictionary command does not contain command key. Received: {dummy_cmd__str}")
+    client_socket.sendall.assert_called_with(
+        f"Invalid test command. Received test dictionary command does not contain command key. Received: {dummy_cmd__str}".encode())
 
 
 def test_sigrok_backend_sends_cmd_to_cli(socket_mock, logging_mock, sigrok_cli_mocker):
     server_socket, client_socket = socket_mock
     sigrok_cli_mock, mock_send_cmd = sigrok_cli_mocker
-    cmd = "Dummy Command"
-    client_socket.recv.return_value = cmd.encode()
+    dummy_cmd_str = json.dumps(command_dict)
+    client_socket.recv.return_value = dummy_cmd_str.encode()
     sigrok_process(1234, log_file_path)
-    mock_send_cmd.assert_called_with(cmd)
+    mock_send_cmd.assert_called_with(dummy_cmd_str)
 
 
 def test_sigrok_backend_answers_done_if_cmd_done(socket_mock, logging_mock, sigrok_cli_mocker):
     server_socket, client_socket = socket_mock
     sigrok_cli_mock, mock_send_cmd = sigrok_cli_mocker
     mock_send_cmd.return_value = {"status": "done"}
-    cmd = "Dummy Command"
-    client_socket.recv.return_value = cmd.encode()
+    dummy_cmd_str = json.dumps(command_dict)
+    client_socket.recv.return_value = dummy_cmd_str.encode()
     sigrok_process(1234, log_file_path)
     client_socket.sendall.assert_called_with("done".encode())
-    mock_send_cmd.assert_called_with(cmd)
+    mock_send_cmd.assert_called_with(dummy_cmd_str)
